b475c676601132b35cf2c33b2690ff0ab0aca856
Rennan Cockles <rennancockles@gmail.com>
2019-01-10 23:26:52 -0200

    Weblogger icon

--- make.bat
+++ make.bat
@@ -2,6 +2,7 @@ python "C:\Python27\Scripts\pyinstaller-script.py" ^
     --log-level WARN ^
     --distpath "dist\win" --workpath "dist\win\build" --specpath "dist\win\spec" ^
     -wFn "Weblogger" ^
+    -i weblogger.ico ^
     weblogger.pyw
 
 rd /S /Q dist\win\build
